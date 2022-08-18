b09a14ade15f1adc8c780c8de8d902f65b8b0aee
iluxa2003 <ilya.yakov.com@gmail.com>
2022-08-18 13:04:45 +0000

    Deploying to gh-pages from @ iluxa2003/movie-site-project@567573050b17483ce1df41fe7905d87bb657c394 🚀

--- index.5a7bb6df.js
+++ index.5a7bb6df.js
@@ -1,2 +1,2 @@
 fetch("https://api.themoviedb.org/3/trending/all/day?api_key=f1c9e198351fb99a7484d861b34f1dff").then((function(e){console.log(e.json())}));
-//# sourceMappingURL=index.1840f460.js.map
+//# sourceMappingURL=index.5a7bb6df.js.map
