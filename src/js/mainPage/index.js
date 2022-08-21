13bda9321fbc63c2632cc91fbdeb4f6c2c19d80c
iluxa2003 <ilya.yakov.com@gmail.com>
2022-08-21 12:26:15 +0300

    re-building into react project

--- src/js/mainPage/index.js
+++ src/js/mainPage/index.js
@@ -1,5 +1,45 @@
-fetch("https://api.themoviedb.org/3/trending/all/day?api_key=f1c9e198351fb99a7484d861b34f1dff")
-.then(response => {
-    console.log(response.json());  
-})
-// .then(posts=>console.log(posts))
+import { fetchTrends } from './trendingFetch';
+
+const trends = document.querySelector('[recent-movies]');
+const movies = fetchTrends();
+// console.log(movies.then(response=>response));
+function movieCaptions(obj) {
+  for (const movie of obj) {
+    const newMovie = `
+    <div class="photo">
+
+<img src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path }" alt="nothing" loading="lazy"/>
+
+<div class="info">
+<p class="info-item">
+<b>For adults:<br>${movie.adults}</b>
+</p>
+<p class="info-item">
+<b>Title:<br>${movie.title || movie.name}</b>
+</p>
+<p class="info-item">
+<b>Original language:<br>${movie.original_language}</b>
+</p>
+<p class="info-item">
+<b>popularity: <br>${movie.popularity}</b>
+</p>
+</div>
+</div>
+              `;
+    trends.insertAdjacentHTML('beforeend', newMovie);
+  }
+}
+async function trendingMovies() {
+  movies.then(response => 
+    // console.log(response)
+    movieCaptions(response.results)
+  );
+}
+trendingMovies();
+// fetchTrends().then(response =>console.log(response))
+// for (const movie of movies) {
+//   const newMovie = `
+
+//               `;
+//   trends.insertAdjacentHTML('beforeend', newMovie);
+// }
--- src/js/mainPage/trendingFetch.js
+++ src/js/mainPage/trendingFetch.js
@@ -3,8 +3,10 @@ const fetchTrends = () => {
   const trends = fetch(
     'https://api.themoviedb.org/3/trending/all/day?api_key=f1c9e198351fb99a7484d861b34f1dff'
   ).then(response => {
-    return console.log(response.json());
+    return response.json();
   });
+
+  return trends
 };
 
-export default fetchTrends;
+export {fetchTrends};
