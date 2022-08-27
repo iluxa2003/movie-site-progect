19e916628ba499fc8d90dbfc7e70bf2034b37ac8
iluxa2003 <ilya.yakov.com@gmail.com>
2022-08-27 22:05:55 +0300

    added pagination

--- src/fetches/moviesFetch.js
+++ src/fetches/moviesFetch.js
@@ -1,10 +1,10 @@
 
 
 
-const FetchTrends = () => {
+const FetchTrends = (page) => {
 
   const trends = fetch(
-    "https://api.themoviedb.org/3/trending/all/day?api_key=f1c9e198351fb99a7484d861b34f1dff"
+    `https://api.themoviedb.org/3/trending/all/day?api_key=f1c9e198351fb99a7484d861b34f1dff&page=${page}`
   ).then((response) => {
     return response.json();
   });
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,16 +3,27 @@ import React, { useState, useEffect } from "react";
 import MovieList from "../components/MainPage/MovieList";
 import Header from "../components/Header/Header"
 const MainPage = () => {
+
+  const [page, setPage] = useState(1)
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    FetchTrends().then((response) => {
+    FetchTrends(page).then((response) => {
       return setMovies(response.results);
     });
-  },[]);
+  },[page]);
+
+  const nextHandler =()=>{
+    setPage(i=>{return i+1});
+  }
+  const prevHandler =()=>{
+    setPage(i=>{return i-1});
+  }
   return (
     <div>
       <Header/>
       <main>
+      <button onClick={prevHandler}>Previous page</button>
+        <button onClick={nextHandler}>Next page</button>
         <MovieList items={movies} />
       </main>
     </div>
